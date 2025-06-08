479201896320a9627ada054072087e7a71a749d8
Shamim <hossains159@gmail.com>
2025-06-08 22:22:44 +0600

    style: format import statements and add comments for clarity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import {
+  // Global,
+  Module,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
+// @Global() // not recommended for most cases, use only if you need a truly global service
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
